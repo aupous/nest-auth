59171ffa061cbfb1770426be2a97170e8feb1489
tuanpham <tuan.phamvan.m@gmail.com>
2020-07-19 14:40:01 +0700

    feat: create user module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './modules/user/user.module';
 import config from './config';
 
 @Module({
@@ -12,6 +13,7 @@ import config from './config';
       useFindAndModify: false,
       useCreateIndex: true,
     }),
+    UserModule,
   ],
   controllers: [AppController],
   providers: [AppService],
