874906eb15c21090509ff31b0d3233f409b7474f
tuanpham <tuan.phamvan.m@gmail.com>
2020-07-19 14:15:14 +0700

    feat: config mongodb

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import config from './config';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(config.mongodb.uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+      useCreateIndex: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
